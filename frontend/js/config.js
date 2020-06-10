8e42f0c48a4f1c48e2a36ea3c8b77fa61a556c23
Monte Misa <montemisa@gmail.com>
2020-06-09 22:16:20 -0600

    updated

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_SHdU8alXy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4ferljbeicsfvts38jhr9btuce', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_y1MBNFVvo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '47t06l2ea8q670unqs0136q2ii', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://obzed8ml1e.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://1q2ll9x1rd.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
